73103c31712ee72d0d5ef5c46e5a75b6cb9f53cb
tomliuauto <tomliuauto@gmail.com>
2017-07-23 22:10:33 -0500

    add check function

--- mod1_solution/app.js
+++ mod1_solution/app.js
@@ -8,10 +8,21 @@
   LunchCheckController.$inject = ['$scope'];
 
   function LunchCheckController($scope) {
-    $scope.message = "this is message";
+    $scope.message = "";
+    $scope.lunchItem = "";
+    // console.log($scope.lunchItem.split(',').length);
 
     $scope.checkLunch = function () {
-      
+      var m = $scope.lunchItem.split(',').length;
+      // console.log("you check the lunch")
+      if ($scope.lunchItem==="") {
+        $scope.message = "Please enter data first"
+      } else if (m < 4) {
+        $scope.message = "Enjoy!"
+      } else {
+        $scope.message = "Too much!"
+      };
+
     };
   }
 
