7d3b71bb85226431c3700915c121711e7878da27
tomliuauto <tomliuauto@gmail.com>
2017-07-28 13:56:15 -0500

    backup app index for singletons check

--- mod2_solution/old/app.js
+++ mod2_solution/old/app.js
@@ -1,68 +1,82 @@
 (function () {
-'use strict';
+  'use strict';
 
-// angular.module('ShoppingListCheckOff', [])
-// .controller('ToBuyController', ToBuyController)
-// .controller('AlreadyBoughtController', AlreadyBoughtController)
-// .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
+  angular.module('ShoppingListCheckOff', [])
+  .controller('ToBuyController', ToBuyController)
+  .controller('AlreadyBoughtController', AlreadyBoughtController)
+  .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-// ToBuyController.$inject = ['ShoppingListCheckOffService'];
-// function ToBuyController(ShoppingListCheckOffService){
-//   var itemAdder = this;
-//
-//   itemAdder.itemName = "";
-//   itemAdder.itemQuantity = "";
-//
-//   itemAdder.addItem = function () {
-//     ShoppingListCheckOffService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
-//   }
-//
-// }
-//
-// AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
-// function AlreadyBoughtController(ShoppingListCheckOffService){
-//
-// }
-//
-//
-// function ShoppingListCheckOffService() {
-//   var service = this;
-//
-//   // List of shopping items
-//   var itemsA = [];
-//   var itemsB = [];
-//
-//   service.addItem = function (itemName, quantity) {
-//     var item = {
-//       name: itemName,
-//       quantity: quantity
-//     };
-//     itemsA.push(item);
-//   };
-//
-//   service.bought = function(itemIdex, name, quantity) {
-//     itemsA.splice(itemIdex, 1);
-//     var itemBought = {
-//       name: name,
-//       quantity:quantity
-//     };
-//     itemsB.push(itemBought);
-//   };
-//
-//
-//   service.removeItem = function (itemIdex) {
-//     items.splice(itemIdex, 1);
-//   };
-//
-//   service.getItemsA = function () {
-//     return itemsA;
-//   };
-//
-//   service.getItemsB = function () {
-//     return itemsB;
-//   };
-//
-//
-// }
+  ToBuyController.$inject = ['ShoppingListCheckOffService'];
+  function ToBuyController(ShoppingListCheckOffService){
+    var itemAdder = this;
+
+    itemAdder.itemName = "";
+    itemAdder.itemQuantity = "";
+
+    itemAdder.addItem = function () {
+        ShoppingListCheckOffService.addItem(itemAdder.itemName, itemAdder.itemQuantity);
+      }
+
+   itemAdder.items = ShoppingListCheckOffService.getToBuyList();
+
+
+   itemAdder.addBoughtList = function(itemIndex){
+     ShoppingListCheckOffService.addBoughtList(itemIndex);
+     ShoppingListCheckOffService.removeToBuyList(itemIndex);
+   }
+
+   itemAdder.boughtOnes = ShoppingListCheckOffService.getBoughtList();
+
+
+  };
+
+  AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+  function AlreadyBoughtController(ShoppingListCheckOffService){
+    var boughtItems = this;
+
+    boughtItems.tobuyList = ShoppingListCheckOffService.getToBuyList();
+
+    boughtItems.items =  ShoppingListCheckOffService.getBoughtList();
+  };
+
+  function ShoppingListCheckOffService() {
+    var service = this;
+
+    // List of toBuy items
+    var toBuyList = [];
+    // List of Bought items
+    var boughtList = [];
+
+    // add item to toBuyList
+    service.addItem = function (itemName, quantity) {
+        var item = {
+          name: itemName,
+          quantity: quantity
+        };
+        toBuyList.push(item);
+      };
+
+      //Get the whole list items
+    service.getToBuyList = function () {
+      return toBuyList;
+    };
+
+      //remove from toBuyList
+    service.removeToBuyList = function (itemIdex) {
+      toBuyList.splice(itemIdex, 1);
+    };
+
+      //add to bought list
+    service.addBoughtList = function (itemIndex) {
+        boughtList.push(toBuyList[itemIndex]);
+        console.log(boughtList);
+      };
+
+      //Get the whole boughtList items
+    service.getBoughtList = function () {
+      return boughtList;
+    };
+
+  };
 
 })();
