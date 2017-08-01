118c4df5bfb44ac72f779ba2c49bebd94df755e3
Tom Liu <tomliuauto@gmail.com>
2017-08-01 05:47:29 +0000

    get foundItems in MenuSearchService

--- mod3_solution/assignment3-starter-code/app.js
+++ mod3_solution/assignment3-starter-code/app.js
@@ -28,16 +28,19 @@ function MenuSearchService ($http){
       method: "GET",
       url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
     }).then(function (response) {
-      console.log(response.data.menu_items);
+      // console.log(response.data.menu_items);
       var foundItems = [];
       response.data.menu_items.forEach(function(item){
         // console.log(item)
         if (item.description.toLowerCase().indexOf('chicken') !== -1){
-          foundItems.push(item)
+          // console.log(item);
+          foundItems.push(item);
+          
         }
-        console.log(foundItems)
+        
       })
-
+        console.log(foundItems);
+        return foundItems;
     })
     .catch(function (error) {
       console.log(error);
