3007b963cb79df0d92e8b8eea85d0f5208db10cc
tomliuauto <tomliuauto@gmail.com>
2017-08-02 14:43:59 -0500

    realize function by one controller

--- mod3_solution/assignment3-starter-code/app.js
+++ mod3_solution/assignment3-starter-code/app.js
@@ -4,21 +4,58 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
+.directive('foundItems', FoundItems);
+
+
+
+function FoundItems() {
+  var ddo = {
+    // scope:{
+    //   items: '<',
+    //   searchTerm: '@'
+    // },
+    // controller: MenuController,
+    // controllerAs: 'Mcontrol',
+    // bindToController: true,
+    templateUrl: 'listItem.html'
+  };
+
+  return ddo;
+}
+
+// function MenuController() {
+//   var Mcontrol = this;
+//   console.log(Mcontrol);
+//   }
+// }
+
+
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 function NarrowItDownController(MenuSearchService){
   var controller = this;
   controller.searchTerm = " ";
-  // console.log("this is Ok")
+  controller.items = [];
+
   controller.search = function (){
     console.log(controller.searchTerm);
-    // console.log("you click the search key");
-    var promise = MenuSearchService.getMatchedMenuItems(controller.searchTerm);
-    console.log(promise);
+    controller.items = MenuSearchService.getMatchedMenuItems(controller.searchTerm);
+    console.log(controller.items);
+
   };
+
+  controller.getItems = function (){
+    return controller.items;
+  }
+
+
+  controller.removeItem = function(itemIndex){
+    controller.items.splice(itemIndex, 1);
+  }
 
 };
 
+//MenuSearchService
 MenuSearchService.$inject = ['$http'];
 function MenuSearchService ($http){
   var service = this;
@@ -26,31 +63,34 @@ function MenuSearchService ($http){
 
   service.getMatchedMenuItems = function(rearchItem){
     // console.log(term);
-    return $http({
+    var foundItems = [];
+    $http({
       method: "GET",
       url: ("https://davids-restaurant.herokuapp.com/menu_items.json")
     }).then(function (response) {
       // console.log(response.data.menu_items);
-      var foundItems = [];
+      // var foundItems = [];
       response.data.menu_items.forEach(function(item){
         // console.log(item)
         if (item.description.toLowerCase().indexOf(rearchItem.toLowerCase()) !== -1){
           // console.log(item);
           foundItems.push(item);
-          
         }
-        
+
       })
-        console.log(foundItems);
+        // console.log(foundItems);
         return foundItems;
     })
     .catch(function (error) {
       console.log(error);
     });
-
-
+    console.log(foundItems);
+    return foundItems;
   }
 
+  // service.removeItem = function (itemIndex) {
+  //   foundItems.splice(itemIndex, 1);
+  // };
 
 };
 
